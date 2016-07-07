11a13ff5bf6c6c9c6e73dd40740e441dd5a5435e
David Jeusette <jeusette.david@gmail.com>
2016-07-07 10:49:10 +0200

    Remove assets tarball when finished

--- lib/assets.sh
+++ lib/assets.sh
@@ -2,7 +2,14 @@ download_assets() {
   wget "$ASSETS_TAR_URL" -O entries.tar.gz
 }
 
+# extract_assets <build_dir>
 extract_assets() {
-  mkdir -p "$ASSETS_OUTPUT_PATH"
-  tar xzf entries.tar.gz -C "$ASSETS_OUTPUT_PATH"
+  local build_dir=$1
+  local assets_dir="$build_dir/$ASSETS_OUTPUT_PATH"
+  mkdir -p "$assets_dir"
+  tar xzf entries.tar.gz -C "$assets_dir"
+}
+
+clean_assets() {
+  rm entries.tar.gz
 }
